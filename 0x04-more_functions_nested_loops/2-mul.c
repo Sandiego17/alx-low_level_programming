31aa47d550f5722df7d5501fc590bf52e10120d9
Henry.Uzor <h.uzor17@gmail.com>
2023-03-24 03:24:56 -0700

    feat:collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * nul - multiplies two integers
+ * mul - multiplies two integers
  * @x: The first number to multiply
  * @y: The second number to multiply with first number
  * Return: The result of multiplication between x param and y param
  */
 
-int nul(int x, int y)
+int mul(int x, int y)
 {
 	return (x * y);
 }
